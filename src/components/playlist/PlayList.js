e07d162e1bcfa380e3f2e6f785ea2840411b84eb
Muhammad Moaz Khan <moazkh60@gmail.com>
2019-10-12 14:20:38 +0500

    App structure and lib setup
    Libraries installed stackNavigator setup. Component directory and PlayList component setup

---
+++ src/components/playlist/PlayList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class PlayList extends Component{
+    render(){
+        return(
+            <View><Text>Hello PlayList</Text></View>
+        )
+    }
+}
+
+export default PlayList;
