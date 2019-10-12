6e074239bebf94e9dfa222163eb685a4525867c4
Muhammad Moaz Khan <moazkh60@gmail.com>
2019-10-12 14:55:49 +0500

    FlatList setup,  listItem and style files created

--- src/components/playlist/PlayList.js
+++ src/components/playlist/PlayList.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import { styles } from './PlayListStyles';
 
 class PlayList extends Component{
+
+    playListData = [
+        {
+          id: '1',
+          title: 'Dummy playlist title 3',
+          image: '',
+          noOfTracks: '20'
+        },
+        {
+            id: '2',
+            title: 'Dummy playlist title 2',
+            image: '',
+            noOfTracks: '10'
+          },
+      ];
+    
     render(){
         return(
-            <View><Text>Hello PlayList</Text></View>
+            <View style={styles.container}>
+                <Text>Hello PlayList</Text>
+                <FlatList
+                  data={playListData}
+                  renderItem={({ item }) => <Item title={item.title} />}
+                  keyExtractor={item => item.id}
+                />
+            </View>
         )
     }
 }
---
+++ src/components/playlist/playListItem/PlayListItem.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export const playListItem = (props) => {
+    retrun (
+        <View>
+            <Text>List Item</Text>
+        </View>
+    )
+}
