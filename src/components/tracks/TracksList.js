029a6fdc74228fafd6fe2e9b10ea4ee7b9065488
Muhammad Moaz Khan <moazkh60@gmail.com>
2019-10-14 10:49:57 +0500

    Trackslist component files setup

---
+++ src/components/tracks/TracksList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text, FlatList } from 'react-native';
+
+class TracksList extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Tracks list</Text>
+            </View>
+        )
+    }
+}
+export default TracksList;
---
+++ src/components/tracks/tracksListItem/TracksListItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TouchableOpacity, View, Text, Image } from 'react-native'; 
+import { styles } from './PlayListItemStyles';
+
+/*
+ * TrackList item for flatlist. Props
+ * will be passed from TracksList class
+ * @param {object} props 
+ */
+const TracksListItem = (props) => {
+    return(
+        <TouchableOpacity>
+            
+        </TouchableOpacity>
+    )
+}
