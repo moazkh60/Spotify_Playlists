eaa4b67a28cb026eb233d3eaf1b9a837309ea54d
Muhammad Moaz Khan <moazkh60@gmail.com>
2019-10-14 16:41:33 +0500

    TrackDetail file created

---
+++ src/components/tracks/trackDetail/TrackDetail.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {View, Alert} from 'react-native';
+import {styles} from '../../common/stylesheet';
+
+class TrackDetail extends Component {
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
+}
+export default TrackDetail;
